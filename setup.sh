8f1deafaa1362ac110fa927edebaf58f68fe0390
goforbroke <go.for.broke1006@gmail.com>
2022-06-23 11:45:33 +0300

    working sample in bash

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@
 sudo apt-get update
 
 sudo apt install -y python3.8-venv
-python3 -m venv ./venv/
-source ./venv/bin/activate
+if [[ ! -d ./venv/ ]]; then
+  python3 -m venv ./venv/
+  source ./venv/bin/activate
+fi
 
 sudo add-apt-repository ppa:luke-jr/bitcoincore -y
 sudo apt-get update
